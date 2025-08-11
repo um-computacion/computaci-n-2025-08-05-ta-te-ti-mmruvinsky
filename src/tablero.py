663f85b2d36bfd6e3718e331b0475f7b25401b60
mmruvinsky <mmruvinsky@gmail.com>
2025-08-11 19:29:30 -0300

    validadores

--- src/tablero.py
+++ src/tablero.py
@@ -1,12 +1,24 @@
-class Tablero:
-    def __init__(self):
-       self.casilleros = [[' ' for _ in range(3)] for _ in range(3)]
+from excepciones import PosicionInvalida, CasilleroOcupado
+from validadores import es_posicion_valida, casillero_libre
 
-       def poner_ficha(self, fila, columna, ficha):
-           if self.__tablero[fila][columna] == ' ':
-               self.__tablero[fila][columna] = ficha
-           else:
-               raise ValueError("Casillero ya ocupado")
+class Tablero:
+    """Representa el tablero del juego"""
+    def __init__(self):
+        self.casilleros = [[' ' for _ in range(3)] for _ in range(3)]
+
+    def poner_ficha(self, fila, columna, ficha):
+        if casillero_libre(self, fila, columna):
+            self.casilleros[fila][columna] = ficha
+        else:
+            raise CasilleroOcupado("Casillero ya ocupado")
            
-       def obtener_casillero(self, fil, col):
-           return self.casilleros[fil][col]
+    def obtener_casillero(self, fil, col):
+        return self.casilleros[fil][col]
+    
+    def __str__(self):
+        resultado = ""
+        for i in range(3):
+            resultado += " | ".join(self.casilleros[i]) + "\n"
+            if i < 2:
+                resultado += "-" * 9 + "\n"
+        return resultado
--- src/validadores.py
+++ src/validadores.py
@@ -1,5 +1,7 @@
+from tablero import Tablero
+
 def es_posicion_valida(fil, col):
-    return 0 < fil <= 3 and 0 < col <= 3
+    return 0 <= fil <= 2 and 0 <= col <= 2
 
 def casillero_libre(tablero, fil, col):
     return tablero.obtener_casillero(fil, col) == ' '
